249e5b61642f2086c0e72aa1c88abf73dc0692ca
Bro-Tech <k2785233247@gmail.com>
2025-09-06 22:37:33 +0800

    了解了Python的文件结构以及一些编写规范, 搞懂了import其他模块以及编译运行__pycache__等问题

---
+++ statics/Python_Reboot/Module_Experiment/main.py
@@ -0,0 +1,3 @@
+# 这里是主函数, 将会引入各种其他的'模块'(py文件)进行调用
+import printer_module
+printer_module.say_hi("Cirno");
---
+++ statics/Python_Reboot/Module_Experiment/printer_module.py
@@ -0,0 +1,4 @@
+def say_hi(username):
+    print("Ciallo~ 我是: " + username);
+
+print("这是一个测试, 看看printer_module中的第四行是否会执行");
