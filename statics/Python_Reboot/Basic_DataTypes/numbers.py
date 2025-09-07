3b223190d0101ff0e15f964e58fc4031cb3bc6fb
Bro-Tech <k2785233247@gmail.com>
2025-09-07 18:19:16 +0800

    追加了各种math库方法的实践

--- statics/Python_Reboot/Basic_DataTypes/numbers.py
+++ statics/Python_Reboot/Basic_DataTypes/numbers.py
@@ -26,6 +26,7 @@ print("int_value取得的值为:", int_value);
 
 # Number内置函数
 # abs()取绝对值, 返回的数据类型 与 输入的一致
+print("\n以下开始为Number内置函数的实验")
 neg_value = -5.5;
 print("abs取到的值为: ", abs(neg_value));
 
@@ -39,3 +40,24 @@ print("num_list中最小的值为", min(num_list), ",最大的值为", max(num_l
 
 # pow() 算指定次幂
 print(pow(2, 4)); # 16
+
+# math库的各种方法
+print("\n以下开始为math库的实验");
+
+# fabs() 也是取绝对值, 但总是返回小数
+print("整数-5从fabs()返回的结果为: ", math.fabs(-5)); # -5.0
+
+# ceil() / floor() 往大/小的"整数"取
+print("4.5的ceil为", math.ceil(round_value), ", floor为", math.floor(round_value)); # 5 4
+
+# exp(x) 计算指数e的x次幂, 得出的结果为float
+print("指数e的0次幂为", math.exp(0)); # 1.0
+
+# log(x, y) 计算'对数', logyX (结果为float), log10(X)则直接以10为底数, 不指定底数时 默认为'e'
+print("log以2为底数, 8的对数为",math.log(8,2)); # 3.0
+
+# modf(), 将输入的Number拆成 小数+整数, 返回一个元组, 
+print("2.334用modf()拆出来的元组为",math.modf(2.334)); # (0.334000...01, 2.0);
+
+# sqrt(), 计算平方根
+print("⑨的平方根为",math.sqrt(9)); # 3
