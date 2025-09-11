1eb48a93931539b682abc665df1e46d13725323c
Bro-Tech <k2785233247@gmail.com>
2025-09-11 15:47:31 +0800

    继续了解了一些辅助操作符, Python字符串怎么这么多花样xwx,,,

--- statics/Python_Reboot/Basic_DataTypes/strings.py
+++ statics/Python_Reboot/Basic_DataTypes/strings.py
@@ -4,7 +4,10 @@ print("%05d" % 14);
 sec_a = "aaaaaa"; # 6个a
 sec_b = "bbb";
 sec_c = "ccccccc";
-# sec_a'预留显示位'为5, 靠右对齐; sec_b则是6个'字符位', 靠左对齐; sec_c是15个'字符位', 靠右对齐
+
+# sec_a'预留显示位'为5, 靠右对齐; 
+# sec_b则是6个'字符位', 靠左对齐; 
+# sec_c是15个'字符位', 靠右对齐;
 print("%5s %-6s - %15s" % (sec_a, sec_b, sec_c));
 # print(f"{sec_a:>5} {sec_b:<6} - {sec_c:>15}") # 发现有一个'多出来'的a (超'预留显示位'了)
 
